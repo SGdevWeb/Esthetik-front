1066fff55690703929ce2568aefea4543784cd03
SGdevWeb <samuel.gustin.dev@gmail.com>
2023-04-22 19:26:40 +0200

    Adding the router for the application

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Content from "./components/Contents";
 import styles from "./App.module.scss";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Tarifs from "./pages/Tarifs/Tarifs";
 
 function App() {
   return (
     <div className={`d-flex flex-column ${styles.appContainer}`}>
-      <Header />
-      <Content />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tarif" element={<Tarifs />} />
+        </Routes>
+      </Router>
       <Footer />
     </div>
   );
