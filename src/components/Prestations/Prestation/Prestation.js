d2d1de138722d8a2a59cac91e5ca41525fd2421a
SGdevWeb <samuel.gustin.dev@gmail.com>
2023-05-20 23:01:44 +0200

    Animation on the different images of the prestations page

--- src/components/Prestations/Prestation/Prestation.js
+++ src/components/Prestations/Prestation/Prestation.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Prestation.module.scss";
 import PrestationCard from "../PrestationCard/PrestationCard";
 import data from "../../../data/tarif.json";
 import { useParams } from "react-router-dom";
 
 function Prestation() {
+  console.log("rendu");
+  const [showImage, setShowImage] = useState(false);
+  console.log(showImage);
   const serviceName = useParams().service.split("_").join(" ");
   const dataService = data.find((service) => service.service === serviceName);
+
+  useEffect(() => {
+    setShowImage(false);
+    const timer = setTimeout(() => {
+      setShowImage(true);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [serviceName]);
 
   return (
     <div className={styles.container}>
@@ -17,12 +29,19 @@ function Prestation() {
           price={prestation.price}
         />
       ))}
-      <img
-        key={dataService.id}
-        src={require(`../../../assets/images/prestation/${dataService.picture}.jpg`)}
-        alt={dataService.picture}
-        loading="lazy"
-      />
+      <div
+        className={`${styles.imagePlaceholder} ${
+          showImage ? styles.hidden : ""
+        }`}
+      ></div>
+      {showImage && (
+        <img
+          key={dataService.id}
+          src={require(`../../../assets/images/prestation/${dataService.picture}.jpg`)}
+          alt={dataService.picture}
+          className={`${styles.image}`}
+        />
+      )}
     </div>
   );
 }
