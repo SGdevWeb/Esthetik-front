99bc30dbf4c6f8a9e9894445e89820f06b6f686f
SGdevWeb <samuel.gustin.dev@gmail.com>
2023-07-29 07:22:21 +0200

    Home page responsive

--- src/components/Prestations/Navigation/Navigation.js
+++ src/components/Prestations/Navigation/Navigation.js
@@ -42,3 +42,64 @@ function Navigation() {
 }
 
 export default Navigation;
+
+/* Test avec la bdd */
+
+// import React, { useEffect, useState } from "react";
+// import styles from "./Navigation.module.scss";
+// import { NavLink } from "react-router-dom";
+// import Dropdown from "../Dropdown/Dropdown";
+// import Forfait from "../Forfait/Forfait";
+// import axios from "axios";
+
+// function Navigation() {
+//   const [tarifs, setTarifs] = useState([]);
+
+//   useEffect(() => {
+//     // Fonction pour effectuer la requête GET vers l'API pour récupérer les tarifs
+//     const fetchTarifs = async () => {
+//       try {
+//         const response = await axios.get("http://localhost:3001/rates"); // Remplacez "/api/tarifs" par l'endpoint réel de votre API pour récupérer les tarifs
+//         console.log(response.data);
+//         setTarifs(response.data);
+//       } catch (error) {
+//         console.error("Erreur lors de la récupération des tarifs :", error);
+//       }
+//     };
+
+//     fetchTarifs();
+//   }, []);
+
+//   return (
+//     <>
+//       <div className={styles.container}>
+//         <ul>
+//           {tarifs.map((tarif) => (
+//             <li key={tarif.id}>
+//               <NavLink
+//                 className={styles.navlink}
+//                 to={`/prestations/${tarif.typeService.split(" ").join("_")}`}
+//               >
+//                 {tarif.typeService}
+//               </NavLink>
+//               {/* <div className={styles.dropdown}>
+//                 <Dropdown title={tarif.service} content={tarif.prestations} />
+//               </div> */}
+//             </li>
+//           ))}
+//           <li>
+//             <NavLink className={styles.navlink} to="/prestations/forfait">
+//               Forfait
+//             </NavLink>
+//             <div className={`${styles.forfaitCard} d-flex flex-column mt-20`}>
+//               <h2 className={styles.sectionTitle}>FORFAIT</h2>
+//               <Forfait />
+//             </div>
+//           </li>
+//         </ul>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Navigation;
