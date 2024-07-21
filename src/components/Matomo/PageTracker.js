ec15179bf99c90e457e56d6d4c5fa1b98881ef66
SGdevWeb <samuel.gustin.dev@gmail.com>
2024-07-21 13:12:00 +0200

    Changing the package manager from npm to Yarn

--- src/components/Matomo/PageTracker.js
+++ src/components/Matomo/PageTracker.js
@@ -1,4 +1,3 @@
-// src/components/PageTracker.js
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { useMatomo } from "react-matomo";
@@ -8,7 +7,17 @@ const PageTracker = () => {
   const location = useLocation();
 
   useEffect(() => {
-    trackPageView();
+    // Log les informations de la page pour débogage
+    console.log("Tracking Page View:");
+    console.log("Title:", document.title);
+    console.log("Path:", location.pathname);
+    console.log("Search Params:", location.search);
+
+    // Suivre la vue de la page avec Matomo
+    trackPageView({
+      documentTitle: document.title,
+      href: location.pathname + location.search,
+    });
   }, [location, trackPageView]);
 
   return null;
